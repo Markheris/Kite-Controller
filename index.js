0f33e718512976856f46ef1d157c78a94b26ac94
Efe Baran Yaman <efebaranymn2002@gmail.com>
2024-03-07 15:52:25 +0200

    cookie

--- index.js
+++ index.js
@@ -23,7 +23,18 @@ import { tournamentRouter } from "./routes/tournament.js";
 const io = new Server(server, {
     cors: { origin: "*" }
 })
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
 app.use(express.json())
 app.use(cors({
     origin: "*"
